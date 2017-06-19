9416237c57db70a0969d0266d5f17d48c472a517
tkless <TGK-fF2-v8c-eCu>
2017-06-19 16:07:01 +0200

    update star_rating.js

--- star_rating/ccm.star_rating.js
+++ star_rating/ccm.star_rating.js
@@ -4,149 +4,185 @@
  * @license The MIT License (MIT)
  */
 ( function () {
-    var ccm_version = '8.0.0';
-    var ccm_url     = '../libs/ccm.js';
-    var component_name = 'star_rating';
-    var component_obj  = {
+  var ccm_version = '8.0.0';
+  var ccm_url     = '../libs/ccm.js';
+  var component_name = 'star_rating';
+  var component_obj  = {
 
-      name: component_name,
-      config: {
-        templates: {
-          "main": {
-            "tag": "div",
-            "class" : "rating",
-            "onclick": "%click%"
-          },
+    name: component_name,
+    config: {
+      templates: {
+        "main": {
+          "class": "main",
+          "inner": [ {
+            "class": "wrapper",
+            "inner": [
+              {
+                "class" : "rating"
+              },
+              {
+                "id": "total-count"
+              }
+            ]
+            },
+            {
+              "tag": "div",
+              "id": "bars"
+            }
+          ]
+        },
 
-          "input": {
-            "tag": "input",
-            "type": "radio",
-            "name": "rating",
-            "id":   "%id%",
-            "value":"%star%"
-          },
+        "input": {
+          "tag": "input",
+          "type": "radio",
+          "name": "rating",
+          "id":   "%id%",
+          "value":"%star%",
+          "onclick": "%click%"
+        },
 
-          "label": {
-            "tag": "label",
-            "class": "%class%",
-            "for": "%for%",
-            "title": "%title%"
+        "label": {
+          "tag": "label",
+          "class": "%class%",
+          "for": "%for%",
+          "title": "%title%"
+        },
+        "bars": {
+          "tag": "div",
+          "class": "container",
+          "inner": {
+            "class": "total"
           }
-        },
-
-        data:  {
-            store: [ 'ccm.store', '../star_rating/datastore.json' ],
-            key:   'demo'
-        },
-        user:  [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/ccm.user.min.js'],
-        style: [ 'ccm.load', '../star_rating/style.css' ],
-        icons: [ 'ccm.load', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'],
-        star_title: [ "Gefällt mir gar nicht", "Gefällt mir gar nicht", "Gefällt mir nicht", "Gefällt mir nicht",
-                      "Ist Ok", "Ist Ok", "Gefällt mir", "Gefällt mir", "Gefällt mir sehr", "Gefällt mir sehr" ]
+        }
       },
 
-      Instance: function () {
-        var self = this;
-        var total = 0;
+      data:  {
+          store: [ 'ccm.store', '../star_rating/datastore.json' ],
+          key:   'demo'
+      },
+      user:  [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/ccm.user.min.js'],
+      style: [ 'ccm.load', '../star_rating/style.css' ],
+      icons: [ 'ccm.load', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'],
+      star_title: [ "Gefällt mir gar nicht", "Gefällt mir gar nicht", "Gefällt mir nicht", "Gefällt mir nicht",
+                    "Ist Ok", "Ist Ok", "Gefällt mir", "Gefällt mir", "Gefällt mir sehr", "Gefällt mir sehr" ]
+    },
 
-        this.init = function ( callback ) {
+    Instance: function () {
+      var self = this;
+      var total = 0;
 
-          // listen to change event of ccm realtime datastore => (re)render own content
-          self.data.store.onChange = function () { self.start(); };
+      this.init = function ( callback ) {
 
-          callback();
-        };
+        // listen to change event of ccm realtime datastore => (re)render own content
+        self.data.store.onChange = function () { self.start(); };
 
-        this.ready = function ( callback ) {
+        callback();
+      };
 
-          // listen to login/logout event => (re)render own content
-          if ( self.user ) self.user.addObserver( function () { self.start(); } );
+      this.start = function ( callback ) {
 
-          callback();
-        };
+        document.head.appendChild( self.ccm.helper.html( {
+           tag:   'style',
+           inner: "@font-face { font-family: 'FontAwesome'; src: url('../libs/font-awesome/fonts/fontawesome-webfont.eot?v=4.7.0'); src: url('../libs/font-awesome/fonts/fontawesome-webfont.eot?#iefix&v=4.7.0') format('embedded-opentype'), url('../libs/font-awesome/fonts/fontawesome-webfont.woff2?v=4.7.0') format('woff2'), url('../libs/font-awesome/fonts/fontawesome-webfont.woff?v=4.7.0') format('woff'), url('../libs/font-awesome/fonts/fontawesome-webfont.ttf?v=4.7.0') format('truetype'), url('../libs/font-awesome/fonts/fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') format('svg'); font-weight: normal; font-style: normal; }"
+        } ) );
 
-        this.start = function ( callback ) {
+        self.ccm.helper.dataset( self.data.store, self.data.key, function ( dataset ) {
+          if ( !dataset ) dataset = {};
 
-          document.head.appendChild( self.ccm.helper.html( {
-             tag:   'style',
-             inner: "@font-face { font-family: 'FontAwesome'; src: url('../libs/font-awesome/fonts/fontawesome-webfont.eot?v=4.7.0'); src: url('../libs/font-awesome/fonts/fontawesome-webfont.eot?#iefix&v=4.7.0') format('embedded-opentype'), url('../libs/font-awesome/fonts/fontawesome-webfont.woff2?v=4.7.0') format('woff2'), url('../libs/font-awesome/fonts/fontawesome-webfont.woff?v=4.7.0') format('woff'), url('../libs/font-awesome/fonts/fontawesome-webfont.ttf?v=4.7.0') format('truetype'), url('../libs/font-awesome/fonts/fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') format('svg'); font-weight: normal; font-style: normal; }"
-          } ) );
+          self.ccm.helper.setContent( self.element, self.ccm.helper.protect( self.ccm.helper.html( self.templates.main ) ) );
 
-          self.ccm.helper.dataset( self.data.store, self.data.key, function ( dataset ) {
-            if ( !dataset ) dataset.star_rating = [];
+          //calculate average of rating
+          var sum = 0;
+          var count = 0;
 
-            self.ccm.helper.setContent( self.element, self.ccm.helper.protect( self.ccm.helper.html( self.templates.main, {
-                click: function() {
-                    doVoting();
-                }
-            } ) ) );
-            
-            renderStars();
+          for ( var key in dataset ) {
+            sum += key* Object.keys( dataset[ key ] ).length;
+            count += Object.keys(dataset[ key ] ).length;
+          }
+          total = sum / count;
 
-            for ( var i = 0.5; i <= 5; i += 0.5 ) {
-              if ( dataset[i] )
-                total += Object.keys(dataset[i]).length;
+          //render html content
+          renderStars();
+          renderBars();
+
+          function renderStars() {
+
+            for ( var i = 5; i >= 0.5; i -= 0.5 ) {
+              self.element.querySelector( '.rating' ).appendChild( self.ccm.helper.protect( self.ccm.helper.html( self.templates.input, {
+                id: i,
+                star: i,
+                click: function() { doVoting(); }
+              } ) ) );
+
+              self.element.querySelector( '.rating' ).appendChild( self.ccm.helper.protect( self.ccm.helper.html( self.templates.label, {
+                class: ( ( i * 2 ) % 2 === 0) ? "full" : "half",
+                for: i,
+                title: ( i === 0.5 ) ? self.star_title[ 0 ] : self.star_title[ ( i * 2 ) - 1 ]
+              } ) ) );
+            }
+
+            self.element.querySelector( '#total-count' ).innerHTML = count;
+
+            if ( total < total + 0.5 )
+              self.element.querySelector( 'input[id = "'+ Math.floor( total ) +'"]' ).checked = true;
+            else
+              self.element.querySelector( 'input[id = "'+ Math.ceil( total ) +'"]' ).checked = true;
+          }
+
+          function renderBars() {
+
+            for ( var i = 5; i >= 1; i-- ){
+              var elem = self.element.querySelector( '#bars' ).appendChild( self.ccm.helper.protect( self.ccm.helper.html( self.templates.bars ) ) );
+              elem.classList.add( i );
+
+              if ( dataset[ i ] )
+                self.element.querySelector( '.'+ i + '> .total').innerHTML = Object.keys( dataset[ i ] ).length;
+            }
+          }
+
+          function doVoting() {
+            if ( !self.user )
+                return;
+
+            self.user.login( function () {
+
+              var checked = self.element.querySelector( 'input[name = "rating"]:checked' ).value;
+
+              var user = self.user.data().key;
+
+              if ( !dataset[ checked ] )
+                dataset[ checked ] = {};
+
+              if ( dataset[ checked ][ user ] ) {
+                  // revert vote
+                  delete dataset[ checked ][ user ];
               }
-            
-            function renderStars() {
+              // not voted
+              else {
 
-                for ( var i = 5; i >= 0.5; i -= 0.5 ) {
-                    self.element.querySelector( '.rating' ).appendChild( self.ccm.helper.protect( self.ccm.helper.html( self.templates.input, {
-                        id: i,
-                        star: i
-                    } ) ) );
+                  // proceed voting
+                  dataset[ checked ][ user ] = true;
+              }
 
-                    self.element.querySelector( '.rating' ).appendChild( self.ccm.helper.protect( self.ccm.helper.html( self.templates.label, {
-                        class: ( ( i * 2 ) % 2 === 0) ? "full" : "half",
-                        for: i,
-                        title: ( i === 0.5 ) ? self.star_title[ 0 ] : self.star_title[ ( i * 2 ) - 1 ]
-                    } ) ) );
-                }
-            }
+              // update dataset for rendering => (re)render own content
+              //self.data.store.set( dataset, function () { console.log(dataset); self.start(); } );
 
-            function doVoting() {
-                if ( !self.user )
-                    return;
+            });
+          }
 
-                self.user.login( function () {
+          if ( callback )callback();
+        } );
 
-                    var checked;
-                    if ( self.element.querySelector( 'input[name = "rating"]:checked' ) !== null ) {
-                        checked = self.element.querySelector( 'input[name = "rating"]:checked' ).value;
-                    }
+      };
 
-                    var user = self.user.data().key;
-
-                    if ( dataset[ checked ][ user ] ) {
-                        // revert vote
-                        delete dataset[ checked ][ user ];
-                    }
-                    // not voted
-                    else {
-
-                        // proceed voting
-                        dataset[ checked ][ user ] = true;
-                    }
-
-                    // update dataset for rendering => (re)render own content
-                    self.data.store.set( dataset, function () { self.start(); } );
-
-                });
-            }
-
-            if ( callback )callback();
-          } );
-
-        };
-
-        this.getVoting = function () {
-         return total;
-        }
-
+      this.getVoting = function () {
+       return total;
       }
-    };
 
-    var namespace = window.ccm && ccm.components[ component_name ]; if ( namespace ) { if ( namespace.ccm_version ) ccm_version = namespace.ccm_version; if ( namespace.ccm_url ) ccm_url = namespace.ccm_url; }
-    if ( !window.ccm || !ccm[ ccm_version ] ) { var tag = document.createElement( 'script' ); document.head.appendChild( tag ); tag.onload = register; tag.src = ccm_url; } else register();
-    function register() { ccm[ ccm_version ].component( component_obj ); }
+    }
+  };
+
+  var namespace = window.ccm && ccm.components[ component_name ]; if ( namespace ) { if ( namespace.ccm_version ) ccm_version = namespace.ccm_version; if ( namespace.ccm_url ) ccm_url = namespace.ccm_url; }
+  if ( !window.ccm || !ccm[ ccm_version ] ) { var tag = document.createElement( 'script' ); document.head.appendChild( tag ); tag.onload = register; tag.src = ccm_url; } else register();
+  function register() { ccm[ ccm_version ].component( component_obj ); }
 }() );
